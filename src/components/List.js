9f45c9919b883dfa2b9b7d53718f56cf1e37240d
CMS <cmsdossantos@gmail.com>
2021-10-15 13:57:53 +0200

    deleted css&readme

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const List = (props) => {
   
-  const displayItems = props.data.list.map(el => <li key={el.key}> {el.title} {el.artist}</li>)
+  const displayItems = props.data.list.map(el => <li key={el.key}> {el.title} - {el.artist}</li>)
   
   // const displayItems = Array.from(props.data.list.map(el => <li key={el.key}> {el.title} {el.artist}</li>))
   
